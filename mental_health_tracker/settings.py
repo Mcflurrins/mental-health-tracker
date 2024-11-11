48d75aea2d07ea97ee89669e15b3dcd1c16661f5
Mcflurrins <flori.andrea.60@gmail.com>
2024-11-11 09:23:36 +0700

    pws2 push

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-!s)j3%n$fbp7w261e76g#d5&=5&*e(^6qvu%8tq$bk$sd7k#8q
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://pbp.cs.ui.ac.id/flori.andrea/mentalhealthtracker", "flori-andrea-mentalhealthtracker.pbp.cs.ui.ac.id", "https://flori-andrea-mentalhealthtracker.pbp.cs.ui.ac.id/", "http://flori-andrea-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://pbp.cs.ui.ac.id/flori.andrea/mentalhealthtracker", "flori-andrea-mentalhealthtracker.pbp.cs.ui.ac.id", "https://flori-andrea-mentalhealthtracker.pbp.cs.ui.ac.id/", "http://flori-andrea-mentalhealthtracker.pbp.cs.ui.ac.id/",
+                 "flori-andrea-upcycleshop.pbp.cs.ui.ac.id"]
 
 
 # Application definition

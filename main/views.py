925cbd50505643f53c9f674c9ffd80d24affdfe6
Mcflurrins <flori.andrea.60@gmail.com>
2024-11-13 12:00:55 +0700

    Tutorial 8 push

--- main/views.py
+++ main/views.py
@@ -1,29 +1,50 @@
-import datetime
-from django.shortcuts import render, reverse, redirect
-from main.forms import MoodEntryForm
-from django.urls import reverse
-from django.utils.html import strip_tags
-from main.models import MoodEntry
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
+
+from django.shortcuts import render, redirect
+from main.forms import MoodEntryForm
+from main.models import MoodEntry
+
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from django.urls import reverse
+import datetime
+
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
+import json
+from django.http import JsonResponse
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
 
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
-
     context = {
+        'npm' : '2306172325',
         'name': request.user.username,
-        'class': 'PBP KKI',
-        'npm': '2306123456',
-        'last_login': request.COOKIES['last_login'],
-
+        'class': 'KKI',
+        'last_login' : request.COOKIES.get('last_login')
     }
 
     return render(request, "main.html", context)
@@ -39,6 +60,29 @@ def create_mood_entry(request):
 
     context = {'form': form}
     return render(request, "create_mood_entry.html", context)
+
+def edit_mood(request,id):
+    # Get mood entry based on id
+    mood = MoodEntry.objects.get(pk = id)
+
+    # Set mood entry as an instance of the form
+    form = MoodEntryForm(request.POST or None, instance=mood)
+
+    if form.is_valid() and request.method == "POST":
+        # Save form and return to home page
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_mood.html", context)
+
+def delete_mood(request, id):
+    # Get mood based on id
+    mood = MoodEntry.objects.get(pk = id)
+    # Delete mood
+    mood.delete()
+    # Return to home page
+    return HttpResponseRedirect(reverse('main:show_main'))
 
 def show_xml(request):
     data = MoodEntry.objects.filter(user=request.user)
@@ -63,57 +107,35 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Your account has been successfully created!')
+            messages.success(request, "Account created successfully")
             return redirect('main:login')
-    context = {'form':form}
-    return render(request, 'register.html', context)
+        
+    context = {'form': form}
+    return render(request, "register.html", context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
-      else:
-        messages.error(request, "Invalid username or password. Please try again.")
-
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
+    
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
-    return response
-
-def edit_mood(request, id):
-    # Get mood entry based on id
-    mood = MoodEntry.objects.get(pk = id)
-
-    # Set mood entry as an instance of the form
-    form = MoodEntryForm(request.POST or None, instance=mood)
-
-    if form.is_valid() and request.method == "POST":
-        # Save form and return to home page
-        form.save()
-        return HttpResponseRedirect(reverse('main:show_main'))
-
-    context = {'form': form}
-    return render(request, "edit_mood.html", context)
-
-def delete_mood(request, id):
-    # Get mood based on id
-    mood = MoodEntry.objects.get(pk = id)
-    # Delete mood
-    mood.delete()
-    # Return to home page
-    return HttpResponseRedirect(reverse('main:show_main'))
+    return redirect('main:login')
 
 @csrf_exempt
 @require_POST
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -42,8 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication', 
+    'corsheaders',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +57,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 ROOT_URLCONF = 'mental_health_tracker.urls'
 
@@ -134,3 +145,4 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
